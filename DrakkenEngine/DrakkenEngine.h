e5f97520f2fa755be0cd8de01f926130ba79a353
Allison Lindner <lindner.allison@gmail.com>
2016-05-11 16:50:15 -0300

    Initial Commit

---
+++ DrakkenEngine/DrakkenEngine.h
@@ -0,0 +1,19 @@
+//
+//  DrakkenEngine.h
+//  DrakkenEngine
+//
+//  Created by Allison Lindner on 11/05/16.
+//  Copyright © 2016 Allison Lindner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DrakkenEngine.
+FOUNDATION_EXPORT double DrakkenEngineVersionNumber;
+
+//! Project version string for DrakkenEngine.
+FOUNDATION_EXPORT const unsigned char DrakkenEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrakkenEngine/PublicHeader.h>
+
+
