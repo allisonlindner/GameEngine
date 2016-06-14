d15a4db6050edde6fa618989ea956dd3db575924
Allison Lindner <lindner.allison@gmail.com>
2016-06-14 01:14:28 -0300

    .

---
+++ DrakkenKit_OSX/DrakkenKit_OSX.h
@@ -0,0 +1,19 @@
+//
+//  DKGame_OSX.h
+//  DKGame_OSX
+//
+//  Created by Allison Lindner on 06/06/16.
+//  Copyright © 2016 Allison Lindner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DKGame_OSX.
+FOUNDATION_EXPORT double DrakkenKit_OSXVersionNumber;
+
+//! Project version string for DKGame_OSX.
+FOUNDATION_EXPORT const unsigned char DrakkenKit_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKGame_OSX/PublicHeader.h>
+
+
